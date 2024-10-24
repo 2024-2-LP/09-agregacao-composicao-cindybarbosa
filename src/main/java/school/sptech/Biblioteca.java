c36b694250250778ff784795354c79501aefbc11
Cindy Oliveira Barbosa <cobarbosa@simpress.com.br>
2024-10-24 09:36:30 -0300

    Atividade de Agregação e Composição

--- src/main/java/school/sptech/Biblioteca.java
+++ src/main/java/school/sptech/Biblioteca.java
@@ -1,5 +1,63 @@
 package school.sptech;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Biblioteca {
+
+    private String nome;
+    private List<Livro> livros;
+
+
+    public Biblioteca(String nome, List<Livro> livros) {
+        this.nome = nome;
+        this.livros = new ArrayList<>();
+    }
+
+    public Biblioteca() {
+
+        this.livros = new ArrayList<>();
+    }
+
+    public void adicionarLivro(Livro livro) {
+        if (livro != null && livro.getTitulo() != null && !livro.getTitulo().isEmpty()) {
+            livros.add(livro);
+        }
+    }
+
+    public void removerLivroPorTitulo(String titulo) {
+        for (int i = 0; i < livros.size(); i++) {
+            if (livros.get(i).getTitulo().equalsIgnoreCase(titulo)) {
+                livros.remove(i);
+                break;
+            }
+        }
+    }
+
+    public Livro buscarLivroPorTitulo(String titulo) {
+        if (titulo == null || titulo.isEmpty()) {
+            return null;
+        }
+        for (Livro livro : livros) {
+            if (livro.getTitulo().equalsIgnoreCase(titulo)) {
+                return livro;
+            }
+        }
+        return null;
+    }
+
+    public Integer contarLivros() {
+
+        return livros.size();
+    }
+
+    public List<Livro> obterLivrosAteAno(Integer ano) {
+        List<Livro> livrosAteAno = new ArrayList<>();
+        for (Livro livro : livros) {
+            if (livro.getDataPublicacao().getYear() <= ano) {
+                livrosAteAno.add(livro);
+            }
+        }
+        return livrosAteAno;
+    }
 
 }
--- src/main/java/school/sptech/Main.java
+++ src/main/java/school/sptech/Main.java
@@ -1,8 +1,35 @@
 package school.sptech;
 
+import java.time.LocalDate;
+
 public class Main {
-
     public static void main(String[] args) {
+        Biblioteca biblioteca = new Biblioteca();
 
+        Livro livro1 = new Livro("A rainha vermelha", "Victoria Aveyard", LocalDate.of(2015, 6, 16));
+        Livro livro2 = new Livro("A arte de viver com simplicidade", "SHUNMYO MASUNO", LocalDate.of(2019, 6, 18));
+
+        Avaliacao avaliacao1 = new Avaliacao("O mundo de Mare Barrow é dividido pelo sangue: vermelho ou prateado. Mare e sua família são vermelhos: plebeus, humildes, destinados a servir uma elite prateada cujos poderes sobrenaturais os tornam quase deuses.", 1500.0);
+
+        Avaliacao avaliacao2 = new Avaliacao("Está tão relacionada a práticas e comportamentos quanto está ligada à reflexão. Em seu livro, ele explica os principais conceitos da vida zen e ensina 100 lições simples para colocá-los em prática.", 1900.0);
+
+        livro1.adicionarAvaliacao(avaliacao1);
+        livro2.adicionarAvaliacao(avaliacao2);
+
+        biblioteca.adicionarLivro(livro1);
+        biblioteca.adicionarLivro(livro2);
+
+        Livro livroEncontrado = biblioteca.buscarLivroPorTitulo("A rainha vermelha");
+        if (livroEncontrado != null) {
+            System.out.println("Livro encontrado: " + livroEncontrado.getTitulo() + ", Autor: " + livroEncontrado.getAutor() + ", Data de publicação: " + livroEncontrado.getDataPublicacao());
+        } else {
+            System.out.println("Livro não encontrado.");
+        }
+
+        if (livroEncontrado != null) {
+            for (Avaliacao avaliacao : livroEncontrado.getAvaliacoes()) {
+                System.out.println("Avaliação: " + avaliacao.getDescricao() + ", Comentário: " + avaliacao.getQtdEstrelas());
+            }
+        }
     }
 }
